f21c28b8c58ba2d6a0c3aa723f8517a97de5df19
Milad Mizani <miladmizani@yahoo.com>
2023-05-08 19:38:06 +0300

    Add client auth service

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"log"
-	"flag"
 	"context"
+	"flag"
+	"log"
 	"time"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/status"
-	"google.golang.org/grpc/codes"
-	"example.com/laptop_store/proto"
+	pb "example.com/laptop_store/proto"
 	"example.com/laptop_store/sample"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-func main(){
+func main() {
 
-	// Get address 
+	// Get address
 	serverAddress := flag.String("address", "", "The server address")
 	flag.Parse()
 	log.Printf("Dial Server %s", *serverAddress)
@@ -23,9 +23,9 @@ func main(){
 	// laptop sample
 	laptop := sample.NewLaptop()
 
-	// Dial to the server 
+	// Dial to the server
 	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
-	if err != nil{
+	if err != nil {
 		log.Fatal("Cannot dial server", err)
 	}
 	laptopClient := pb.NewLaptopServiceClient(conn)
@@ -34,18 +34,18 @@ func main(){
 		Laptop: laptop,
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1000 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
 	defer cancel()
-	// call the CreateLaptop function 
+	// call the CreateLaptop function
 	res, err := laptopClient.CreateLaptop(ctx, req)
-	if err != nil{
+	if err != nil {
 		st, ok := status.FromError(err)
-		if ok && st.Code() == codes.AlreadyExists{
+		if ok && st.Code() == codes.AlreadyExists {
 			log.Printf("laptop already exists")
-		}else{
-			log.Fatal("cannot create laptop")
+		} else {
+			log.Fatal("cannot create laptop", err)
 		}
 		return
 	}
-	log.Printf("Laptop is created with: %s id",res.Id)
+	log.Printf("Laptop is created with: %s id", res.Id)
 }
